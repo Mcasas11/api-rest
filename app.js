66e7183183fcd1aca563f26cba3e832eaf723e82
Mcasas11 <marcos.casas.f@gmail.com>
2024-05-23 12:09:42 -0400

    Update Accepted Origins

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ app.use(cors({
       'http://localhost:8080',
       'http://localhost:1234',
       'https://movies.com',
-      'https://midu.dev'
+      'https://midu.dev',
+      'https://api-rest-lilac-xi.vercel.app'
     ]
 
     if (ACCEPTED_ORIGINS.includes(origin)) {
